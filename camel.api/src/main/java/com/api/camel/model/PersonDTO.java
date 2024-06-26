bcf3a71b10234ff29a6d696a7a118cdf9d83faa6
fabiano <fabiannosanttos@gmail.com>
2024-06-26 17:07:48 -0300

    add PersonDTO or PersonService

--- camel.api/src/main/java/com/api/camel/model/PersonDTO.java
+++ camel.api/src/main/java/com/api/camel/model/PersonDTO.java
@@ -1,2 +1,28 @@
-package com.api.camel.model;public class PersonDTO {
+package com.api.camel.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PersonDTO {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("age")
+    public String getAge() {
+        return age;
+    }
+
+    @JsonProperty("age")
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
--- camel.api/src/main/java/com/api/camel/service/PersonService.java
+++ camel.api/src/main/java/com/api/camel/service/PersonService.java
@@ -2,12 +2,14 @@ package com.api.camel.service;
 
 import com.api.camel.exception.ResourceNotFoundException;
 import com.api.camel.model.Person;
+import com.api.camel.model.PersonDTO;
 import com.api.camel.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -15,36 +17,48 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
-    public Person getPerson(Long id) {
-        Optional<Person> personId = personRepository.findById(id);
-        return personId.orElse(null);
-
+    public PersonDTO getPerson(Long id) {
+        Optional<Person> person = personRepository.findById(id);
+        return person.map(this::convertToDto).orElse(null);
     }
 
-    public List<Person> getAllPersons() {
-        return personRepository.findAll();
+    public PersonDTO addPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return convertToDto(savedPerson);
     }
 
-    public Person addPerson(Person person) {
-        return personRepository.save(person);
-    }
-
-    public Person updatePerson(Long id, Person newPerson) {
-        if (personRepository.existsById(id)) {
-            newPerson.setId(id);
-            return personRepository.save(newPerson);
+    public PersonDTO updatePerson(Long id, Person updatedPerson) {
+        Optional<Person> existingPerson = personRepository.findById(id);
+        if (existingPerson.isPresent()) {
+            Person person = existingPerson.get();
+            person.setName(updatedPerson.getName());
+            person.setAgeInWeeks(updatedPerson.getAgeInWeeks());
+            Person savedPerson = personRepository.save(person);
+            return convertToDto(savedPerson);
         } else {
-            throw new ResourceNotFoundException("Person not found with id " + id);
+            throw new ResourceNotFoundException("Person not found with id: " + id);
         }
     }
 
     public void deletePerson(Long id) {
-        try {
-            personRepository.existsById(id);
+        if (personRepository.existsById(id)) {
             personRepository.deleteById(id);
-        } catch (Exception e) {
-            throw new ResourceNotFoundException("Error delete" + id);
+        } else {
+            throw new ResourceNotFoundException("Person not found with id: " + id);
         }
+    }
+
+    public List<PersonDTO> getAllPersons() {
+        return personRepository.findAll().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private PersonDTO convertToDto(Person person) {
+        PersonDTO dto = new PersonDTO();
+        dto.setName(person.getName());
+        dto.setAge(person.getAge());
+        return dto;
     }
 }
 
